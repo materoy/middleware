561f461842719509daa850019ae2977a3e2063a4
Roy M <roymatero@gmail.com>
2021-06-12 15:14:46 +0300

    connects now

---
+++ client.py
@@ -0,0 +1,21 @@
+import socket
+
+HOST = '127.0.0.1'  
+PORT = 8081  
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def conect_server(host, port):
+    sock.connect((host, port))
+
+def send_data(data):
+    data = bytes(data, 'utf-8')
+    sock.sendall(data)
+
+def receive_data():
+    data = sock.recv(1024)
+    return data
+
+if __name__ == "__main__":
+    conect_server(HOST, PORT)
+    send_data("SELECT * FROM Languages")
+    data = receive_data()
--- server.py
+++ server.py
@@ -7,52 +7,49 @@ import sys
 
 from middleware import Database
 
-HOST = "localhost"
-PORT = 8080
-exit = False
+HOST = "127.0.0.1"
+PORT = 8081
 
 db = Database("middleware_db.db")
 
-class Server(socketserver.TCPServer):
-    def __init__(self, server_address: tuple[str, int], RequestHandlerClass: Callable[..., socketserver.BaseRequestHandler], bind_and_activate: bool) -> None:
-        super().__init__(server_address, RequestHandlerClass, bind_and_activate=bind_and_activate)
+# class Server(socketserver.TCPServer):
+#     def __init__(self, server_address: tuple[str, int], RequestHandlerClass: Callable[..., socketserver.BaseRequestHandler], bind_and_activate: bool) -> None:
+#         super().__init__(server_address, RequestHandlerClass, bind_and_activate=bind_and_activate)
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 def server(host, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((host, port))
     sock.listen()
+    while True:
+        try:
+            conn, address = sock.accept()
+            print(f"Connected to {address}")
 
-    conn, address = sock.accept()
-    print(f"Connected to {address}")
-
-    try:
-        while True :
             data = conn.recv(1024)
             if not data:
-                break
-    except KeyboardInterrupt:
-        sys.exit()
+                print(f"Invalid data {data}")
+        except KeyboardInterrupt:
+            print("Exiting ...")
+            break 
+        pass
 
 
 def query_database(query):
+    query = query.decode('utf-8')
     db.perform_query(query)
 
 
 if __name__ == "__main__":      
 
     # sock_server = Server((HOST, PORT), Callable[..., socketserver.BaseRequestHandler], True)  
-    print("Server started http://%s:%s" % (HOST, PORT + 1))
+    print("Server started http://%s:%s" % (HOST, PORT ))
 
-    db = Database("middleware_db.db")
+    # db = Database("middleware_db.db")
 
 
-    try:
-        # socket_server.serve_forever()
-        server(HOST, PORT)
-        pass
-    except KeyboardInterrupt:
-        pass
+   
+    server(HOST, PORT)
+   
     
-    del db
-    exit = True
+    # del db
     print("Server stopped.")
